5327d1bf1785c0b52a82b3bb4563764d690f36eb
sydneyyyyy <portersyd5@gmail.com>
2021-05-24 17:23:23 -0400

    added JavaDay9

---
+++ JavaDay9/src/main/java/com/revature/collections/SortingPlayground.java
@@ -0,0 +1,120 @@
+package com.revature.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/*
+ * Collections: a class filled with static methods used to manipulate collections
+ * 
+ * NOT to be confused with Collection -> which is an interface that provides the standard functionality of a collection
+ * 		this is sometimes also referred to as the Collection API (Application Programming Interface)
+ * 
+ * 
+ * */
+
+public class SortingPlayground  {
+
+	public static void main(String[] args) {
+		
+		// We also have an Arrays Class
+		int[] intArr = {6, 3, 52, 44, 66, 87, 2, 2333, 77, 0, 7, 21, 1, 5};
+		System.out.println("Before sorting...");
+		
+		for (int i: intArr) {
+			System.out.println(i);
+		}
+		
+		Arrays.sort(intArr);
+		
+		System.out.println("\nAfter sorting....");
+		
+		for (int i: intArr) {
+			System.out.println(i);
+		}
+		
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(5);
+		intList.add(8);
+		intList.add(0);
+		
+		System.out.println("\nIntList Before Sort....");
+		for (int i = 0; i < intList.size(); i++) {
+			System.out.println(intList.get(i));
+		}
+		
+		
+		System.out.println("\nIntList after sort....");
+		Collections.sort(intList);
+		for (int i = 0; i < intList.size(); i++) {
+			System.out.println(intList.get(i));
+		}
+		
+		/*
+		 * Both TreeSet and TreeMap order the insertion of objects into the collection based
+		 * on a sorting method.
+		 * 
+		 * 		Whether it be implicit (String, primitives)
+		 * 		Or explicit (implementing Comparable or passing an instance of a Comparator to the Constructor)
+		 * 
+		 * TreeSet and TreeMap sort at the moment of insertion.
+		 * 
+		 * */
+		
+		Set<String> myExampleTreeSet = new TreeSet<>();
+		myExampleTreeSet.add("hello");
+		myExampleTreeSet.add("1hello");
+		myExampleTreeSet.add("!hello");
+		myExampleTreeSet.add(" world");
+		myExampleTreeSet.add("word");
+		myExampleTreeSet.add("Word");
+		myExampleTreeSet.add("wOrd");
+		myExampleTreeSet.add("word!");
+		
+		System.out.println("\nTreeSet....");
+		for (String s: myExampleTreeSet) {
+			System.out.println(s);
+		}
+		
+		// key - value pairs
+		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+		
+		treeMap.put(1, "One");
+		treeMap.put(2, "Two");
+		treeMap.put(3, "Three");
+		treeMap.put(4, "Four");
+		treeMap.put(5, "Five");
+		
+		System.out.println("\nSo do TreeMaps");
+		
+		// Map does not implement iterable
+//		for (Integer i : treeMap.keySet()) {
+//			System.out.println(i + " => " + treeMap.get(i));
+//		}
+		
+		// Sorts the treeMap as we are inserting them
+		Iterator it = treeMap.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<Integer, String> entry = (Map.Entry) it.next();
+			System.out.println(entry.getKey() + " => " + entry.getValue());
+		}
+		
+		Map<User, Integer> umap = new TreeMap<User, Integer>();
+		umap.put(new User("John", 55), 1001);
+		umap.put(new User("Steve", 35), 54848);
+		
+		umap.forEach((user, num) -> System.out.println(user + " => " + num));
+		
+		
+	}
+	
+	
+}
---
+++ JavaDay9/src/main/java/com/revature/collections/User.java
@@ -0,0 +1,29 @@
+package com.revature.collections;
+
+public class User implements Comparable<User> {
+
+	public String name;
+	public int age;
+	
+	public User() {}
+	
+	public User(String name, int age) {
+		this.name = name;
+		this.age = age;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "User [name=" + this.name + ", age=" + this.age + "]";
+	}
+
+	// compareTo() looks at each element of the collection
+	// and returns either -1, 0, or 1 to determine the order
+	
+	@Override
+	public int compareTo(User u) {
+		return this.age - u.age;
+	}
+	
+}
