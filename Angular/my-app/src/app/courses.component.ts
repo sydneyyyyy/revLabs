ca11800ee8ec46da6a2aaf9c25e7657a82f29d55
sydneyyyyy <portersyd5@gmail.com>
2021-06-21 20:13:06 -0400

    added Angular project

---
+++ Angular/my-app/src/app/courses.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-courses',
+    template: `
+        <h2>Courses</h2>
+        <ul>
+            <li>Course 1</li>
+            <li>Course 2</li>
+            <li>Course 3</li>
+        </ul>
+    `
+})
+export class CoursesComponent {
+
+}
